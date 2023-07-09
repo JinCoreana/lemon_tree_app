15cdb521ef9c51468226b42bf4c1eed2a8a1cb50
Jin Choi <jin.choi@sage.com>
2023-07-09 22:21:36 +0200

    stacknavigation

--- App.js
+++ App.js
@@ -4,15 +4,33 @@ import LemonTreeHeader from "./components/LemonTreeHeader";
 import LemonTreeFooter from "./components/LemonTreeFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
 import MenuList from "./components/MenuList";
+import FeedbackForm from "./components/FeedbackForm";
+import LoginPage from "./components/LoginPage";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <>
       <View style={styles.container}>
         <LemonTreeHeader />
-        <WelcomeScreen />
-      </View>
-      <View style={styles.footerContainer}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="LogIn"
+            screenOptions={{ headerStyle: { backgroundColor: "#FBDABB" } }}
+          >
+            <Stack.Screen
+              name="Welcome"
+              component={WelcomeScreen}
+              options={{ title: "Home" }}
+            />
+            <Stack.Screen name="Menu" component={MenuList} />
+            <Stack.Screen name="LogIn" component={LoginPage} />
+            <Stack.Screen name="Feedback" component={FeedbackForm} />
+          </Stack.Navigator>
+        </NavigationContainer>
+
         <LemonTreeFooter />
       </View>
     </>
--- components/MenuList.js
+++ components/MenuList.js
@@ -1,27 +1,159 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, StyleSheet, SectionList, Pressable } from "react-native";
+import LemonTreeFooter from "./LemonTreeFooter";
+import LemonTreeHeader from "./LemonTreeHeader";
+import { useState } from "react";
 
 const menuItemsToDisplay = [
-  "Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta",
+  {
+    title: "Appetizers",
+    data: [
+      { name: "Hummus", price: "$5.00" },
+      { name: "Moutabal", price: "$5.00" },
+      { name: "Falafel", price: "$7.50" },
+      { name: "Marinated Olives", price: "$5.00" },
+      { name: "Kofta", price: "$5.00" },
+      { name: "Eggplant Salad", price: "$8.50" },
+    ],
+  },
+  {
+    title: "Main Dishes",
+    data: [
+      { name: "Lentil Burger", price: "$10.00" },
+      { name: "Smoked Salmon", price: "$14.00" },
+      { name: "Kofta Burger", price: "$11.00" },
+      { name: "Turkish Kebab", price: "$15.50" },
+    ],
+  },
+  {
+    title: "Sides",
+    data: [
+      { name: "Fries", price: "$3.00", id: "11K" },
+      { name: "Buttered Rice", price: "$3.00" },
+      { name: "Bread Sticks", price: "$3.00" },
+      { name: "Pita Pocket", price: "$3.00" },
+      { name: "Lentil Soup", price: "$3.75" },
+      { name: "Greek Salad", price: "$6.00" },
+      { name: "Rice Pilaf", price: "$4.00" },
+    ],
+  },
+  {
+    title: "Desserts",
+    data: [
+      { name: "Baklava", price: "$3.00" },
+      { name: "Tartufo", price: "$3.00" },
+      { name: "Tiramisu", price: "$5.00" },
+      { name: "Panna Cotta", price: "$5.00" },
+    ],
+  },
 ];
 
+const Item = ({ name, price }) => (
+  <View style={styles.innerContainer}>
+    <Text style={styles.contentText}>{name}</Text>
+    <Text style={styles.contentText}>{price}</Text>
+  </View>
+);
+
+const Separator = () => <View style={styles.separator} />;
+
 export default function MenuList() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+  const renderSectionHeader = ({ section: { title } }) => (
+    <View style={styles.headerStyle}>
+      <Text style={styles.sectionHeader}>{title}</Text>
+    </View>
+  );
+  const listHeader = () => {
+    <Text style={styles.titleText}>View Menu</Text>;
+  };
   return (
-    <View style={{ flex: 0.75 }}>
-      <ScrollView
-        indicatorStyle="white"
-        style={{
-          paddingHorizontal: 40,
-          paddingVertical: 40,
-          backgroundColor: "black",
+    <View style={styles.container}>
+      {!showMenu && (
+        <Text style={styles.infoSection}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. View our
+          menu to explore our cuisine with daily specials!
+        </Text>
+      )}
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          setShowMenu(!showMenu);
         }}
       >
-        <Text style={{ color: "white", fontSize: 40, flexWrap: "wrap" }}>
-          View Menu
-        </Text>
-        <Text style={{ color: "#F4CE14", fontSize: 36 }}>
-          {menuItemsToDisplay[0]}
-        </Text>
-      </ScrollView>
+        <Text style={styles.buttonText}>{showMenu ? "Home" : "View Menu"}</Text>
+      </Pressable>
+      <SectionList
+        sections={menuItemsToDisplay}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => item + index}
+        ListFooterComponent={<LemonTreeFooter />}
+        ListHeaderComponent={listHeader}
+        renderSectionHeader={renderSectionHeader}
+        SectionSeparatorComponent={Separator}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingHorizontal: 40,
+    paddingVertical: 40,
+  },
+  sectionHeader: {
+    color: "black",
+    fontSize: 26,
+    flexWrap: "wrap",
+    textAlign: "center",
+  },
+  titleText: {
+    color: "white",
+    fontSize: 30,
+    flexWrap: "wrap",
+  },
+  headerStyle: {
+    backgroundColor: "#F4CE14",
+  },
+  innerContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  contentText: {
+    color: "#F4CE14",
+    fontSize: 20,
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderColor: "#EDEFEE",
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 40,
+    backgroundColor: "#EDEFEE",
+    borderColor: "#EDEFEE",
+    borderWidth: 2,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: "#333333",
+    textAlign: "center",
+    fontSize: 32,
+  },
+  infoSection: {
+    fontSize: 18,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+});
