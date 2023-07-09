86b02e729b5a80e71b4142214de364e7a60448ea
Jin Choi <jin.choi@sage.com>
2023-07-09 02:36:36 +0200

    Introduction to React Native

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LemonTreeHeader from "./components/LemonTreeHeader";
+import LemonTreeFooter from "./components/LemonTreeFooter";
+import WelcomeScreen from "./components/WelcomeScreen";
+import MenuList from "./components/MenuList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <LemonTreeHeader />
+        <WelcomeScreen />
+      </View>
+      <View style={styles.footerContainer}>
+        <LemonTreeFooter />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333333",
   },
+  footerContainer: { backgroundColor: "#333333" },
 });
